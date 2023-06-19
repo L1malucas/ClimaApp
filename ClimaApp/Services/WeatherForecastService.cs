d5e7a398d6292ce22a6468d3e6698f91003e42c6
lucas lima <lucaslj@ufba.br>
2023-06-19 10:09:59 -0300

    Add project files.

---
+++ ClimaApp/Services/WeatherForecastService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+public class WeatherForecastService
+{
+    private readonly HttpClient httpClient;
+    private const string ApiKey = "6c3f1bc597dc52aaaf5f221f09e6a532"; 
+
+    public WeatherForecastService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task<string> GetWeatherForecast(string cityName)
+    {
+        var url = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={ApiKey}";
+        var response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
